07195295d984291a4240926311d111827ad73a79
Fernando Jorge <hello@360play.pt>
2019-05-01 20:42:19 +0100

    Update 15-getJava.sh

--- dependencies/15-getJava.sh
+++ dependencies/15-getJava.sh
@@ -5,8 +5,9 @@ echo "Getting Java 8..."
 DIR=/usr/local/java
 sudo mkdir -p $DIR
 cd $DIR
- sudo wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz
-
+ #sudo wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz
+ sudo wget https://github.com/frekele/oracle-java/releases/download/8u181-b13/jdk-8u181-linux-x64.tar.gz
+ 
  echo "Configuring Java..."
  sudo tar -zxf jdk-8u181-linux-x64.tar.gz
 
